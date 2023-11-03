f3ce7eef15a8d1ade3030fc5efb5282576857ae3
Azin Alahkhani <aalahkhani@gmail.com>
2023-11-03 20:54:27 +0000

    add zoom controller
    zoom controller from kivanc's code + changed the rotation to right click mouse

--- Assets/UI/scripts/PopUp.cs
+++ Assets/UI/scripts/PopUp.cs
@@ -1,10 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 namespace EasyUI.Dialogs{
+    public class Dialog{
+        public string title;
+        public string message;
+    }
+
+   
+
     public class PopUp : MonoBehaviour
     {
+        [SerializeField] GameObject canvas;
+        [SerializeField] Text titleUIText;
+        [SerializeField] Text messageUIText;
+        [SerializeField] Button closeUIButton;
+
+         Dialog dialog = new Dialog();
+
+        public static PopUp Instance;
+
+         void Awake() {
+            Instance = this;
+
+            closeUIButton.onClick.RemoveAllListeners();
+            closeUIButton.onClick.AddListener(Hide);
+        }
+        public PopUp SetTitle(string t){
+            dialog.title = t ;
+            return Instance;
+        }
+        public PopUp SetMessage(string m){
+            dialog.message = m ;
+            return Instance;
+        }
+
+        public void Show(){
+            titleUIText.text = dialog.title;
+            messageUIText.text = dialog.message;
+
+            canvas.SetActive(true);
+            
+        }
+
+        public void Hide(){
+
+            canvas.SetActive(false);
+            
+            dialog = new Dialog();
+            
+        }
+
     
     }
 }
---
+++ Assets/ZoomController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomController : MonoBehaviour
+{
+    public Slider zoomSlider;
+    public Camera mainCamera;
+    public float zoomSpeed = 5f;
+
+    void Update()
+    {
+        float zoomValue = zoomSlider.value;
+
+        // Kamera yakınlaştırma ve uzaklaştırma işlemleri
+        mainCamera.fieldOfView = Mathf.Lerp(20, 60, zoomValue);
+    }
+
+}
